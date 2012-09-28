7181a5df50294979876a04d4a66a4ed7a92cd694
F16Gaming <adam.hellberg95@gmail.com>
2012-09-28 20:03:21 +0200

    Add special text classes.
    For use with in-game text displayed based on map triggers.

---
+++ F16Gaming.Game.RPGLibrary/Engine/Text.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace F16Gaming.Game.RPGLibrary.Engine
+{
+	public class Text
+	{
+		public const float FadeMod = 0.05f;
+		public const float FadeDelay = 0.01f;
+
+		private readonly Rectangle _bounds;
+
+		private float _opacity;
+
+		public string Name { get; private set; }
+		public string Type { get; private set; }
+		public string Title { get; private set; }
+		public string SubTitle { get; private set; }
+
+		public bool Active;
+		public float Mod;
+		public float Elapsed;
+
+		public float Opacity
+		{
+			get { return _opacity; }
+			set { _opacity = MathHelper.Clamp(value, 0.0f, 1.0f); }
+		}
+
+		public Text(Rectangle bounds, string name, string type, string title, string subtitle = null)
+		{
+			_bounds = new Rectangle(bounds.X, bounds.Y, bounds.Width, bounds.Height);
+			Name = name;
+			Type = type;
+			Title = title;
+			SubTitle = subtitle;
+			Active = false;
+			Opacity = 0.0f;
+			Mod = FadeMod;
+			Elapsed = 0.0f;
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			if (!Active || (Mod > 0.0f && Opacity >= 1.0f) || (Mod < 0.0f && Opacity <= 0.0f))
+				return;
+
+			Elapsed += (float) gameTime.ElapsedGameTime.TotalSeconds;
+			if (Elapsed >= FadeDelay)
+			{
+				Elapsed -= FadeDelay;
+				Opacity += Mod;
+			}
+		}
+
+		public bool Intersects(Rectangle rect)
+		{
+			return rect.Intersects(_bounds);
+		}
+	}
+}
---
+++ F16Gaming.Game.RPGLibrary/Engine/TextManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+using F16Gaming.Game.RPGLibrary.Engine.Mapping;
+
+namespace F16Gaming.Game.RPGLibrary.Engine
+{
+	public class TextManager
+	{
+		private List<Text> Texts;
+
+		public TextManager(Map map)
+		{
+			var log = F16Gaming.Game.RPGLibrary.Logging.LogManager.GetLogger(this);
+			log.Info("TextManager is loading map text entries...");
+			Texts = new List<Text>();
+
+			if (!map.HasLayer("text"))
+			{
+				log.Info("No text layer detected on map, aborting...");
+				return;
+			}
+
+			foreach (var obj in map.FindObjects((l, o) => l.Name == "text"))
+			{
+				string title;
+				if (obj.Properties.ContainsKey("title"))
+					title = obj.Properties["title"];
+				else
+					title = "<No Title>";
+
+				string subtitle;
+				if (obj.Properties.ContainsKey("subtitle"))
+					subtitle = obj.Properties["subtitle"];
+				else
+					subtitle = null;
+
+				log.DebugFormat("Adding new text {0} of type {1}", obj.Name, obj.Type);
+				Texts.Add(new Text(obj.Bounds, obj.Name, obj.Type, title, subtitle));
+			}
+		}
+
+		public Text GetIntersecting(Rectangle rect)
+		{
+			return Texts.FirstOrDefault(t => t.Intersects(rect));
+		}
+	}
+}
