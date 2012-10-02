d0a1308c5c5e3cda97fecbb895e2237183b804b8
F16Gaming <adam.hellberg95@gmail.com>
2012-10-03 01:39:02 +0200

    Moved Number.cs to RPGLibrary.Extensions (remodeled to extend integer types)

---
+++ F16Gaming.Game.RPGLibrary/Extensions/Number.cs
@@ -0,0 +1,39 @@
+﻿namespace F16Gaming.Game.RPGLibrary.Extensions
+{
+	public static class Number
+	{
+#region (U)Int*::IsMultiple
+		public static bool IsMultiple(this int num, int mult = 2)
+		{
+			while (num > 2)
+				num /= mult;
+
+			return num == mult;
+		}
+
+		public static bool IsMultiple(this uint num, uint mult = 2)
+		{
+			while (num > 2)
+				num /= mult;
+
+			return num == mult;
+		}
+
+		public static bool IsMultiple(this long num, long mult = 2)
+		{
+			while (num > 2)
+				num /= mult;
+
+			return num == mult;
+		}
+
+		public static bool IsMultiple(this ulong num, ulong mult = 2)
+		{
+			while (num > 2)
+				num /= mult;
+
+			return num == mult;
+		}
+#endregion (U)Int*::IsMultiple
+	}
+}
